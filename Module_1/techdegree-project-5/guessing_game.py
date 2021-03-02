7672c8095a35461ff2651cb66c07d34748810cc0
RSA000 <andisonsully@hotmail.com>
2021-03-02 13:38:10 -0800

    Fixed user input issues and error handling and also added feature to handle users guessing out of range.
    Hoping in the future to add a scoreboard where users can add their names to the highscore

--- Module_1/techdegree-project-5/guessing_game.py
+++ Module_1/techdegree-project-5/guessing_game.py
@@ -1,29 +1,22 @@
 import random
+import os
 
 from time import sleep
-from os import system, name 
+
 # I tried making a flashing screen at the end 
 # using this resource https://www.codespeedy.com/clear-screen-in-python/ 
 
-
-# This section is used to set the the difficulty
-highscore = 1000
-low = 1
-difficulty = int(input("Please choose your difficulty. 1 is the lowest, 3 is the highest: "))
-system('clear')
-
-if difficulty == 1:
-    high = 10
-elif difficulty == 2:
-    high = 100
-else:
-    high = 250
-
+# this is for the ending message. 
 def ending_message(string, exc=5):
+    """
+    This function takes a string as an input prints to screen with
+    increasing exclamations (the amount of following exclamations can be
+    change but increasing/descreasing exc)
+    """
     for i in range(exc):
         print(string+('!'*i))
         sleep(0.3)
-    system('clear')
+    os.system('cls' if os.name == 'nt' else 'clear')
     ending_message(string, exc=5)
 
 
@@ -44,6 +37,7 @@ def start_game(low=0, high=0):
     print('==',highscore,'==')
     print('==================')
     
+    
     while guess != answer:
         try:
             guess = int(input('Type your guess now: '))
@@ -51,11 +45,15 @@ def start_game(low=0, high=0):
             print('You must type a number :p')
             continue
         tries += 1
-        if guess > answer:
-            print('Too High! Guess again.')
+        if guess > answer and guess > high:
+            print('Way too high. number out of guessing range \n')  
+        elif guess > answer:
+            print('Too High! Guess again \n.')
+        elif guess < answer and guess < low:
+            print('Way too low! number out of guessing range \n')
         elif guess < answer:
-            print('Too low! Guess again.')
-    system('clear')
+            print('Too low! Guess again. \n')
+    os.system('cls' if os.name == 'nt' else 'clear')
     
     print(f'Correct! You guessed the number in {tries} tries')
     if highscore > tries:
@@ -63,17 +61,45 @@ def start_game(low=0, high=0):
         highscore = tries
 
     # This runs after the first game is played and allows you to change the difficulty also
-    if input("Would you like to play again? y/n ") == 'y'.lower():
-        difficulty = int(input('Please Choose your difficulty: '))
-        if difficulty == 1:
-            high = 10
-        elif difficulty == 2:
-            high = 100
-        else:
-            high = 250
-
+    if input("Would you like to play again? y/n ") == 'y'.lower(): 
+        while True:
+            difficulty = input('Please Choose your difficulty: ')
+            if difficulty == '1':
+                high = 10
+                break
+            elif difficulty == '2':
+                high = 100
+                break
+            elif difficulty == '3':
+                high = 250
+                break
+            else:
+                print('Sorry thats not a valid input :( ')
+            continue
         start_game(low, high)
     else:
         ending_message('Thanks for playing', exc=5)
 
+
+# This section is used to set the the difficulty
+highscore = 1000
+low = 1
+while True:
+    difficulty = input("Please choose your difficulty. 1 is the lowest, 3 is the highest: ")
+    os.system('cls' if os.name == 'nt' else 'clear')
+    
+    if difficulty == '1':
+        high = 10
+        break
+    elif difficulty == '2':
+        high = 100
+        break
+    elif difficulty == '3':
+        high = 250
+        break
+    else:
+        print("Oh no! Thats not a valid input D: ")
+        continue
+
+    
 start_game(low,high)
